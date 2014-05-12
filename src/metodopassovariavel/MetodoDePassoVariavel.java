80849d0036239378fb8ab08d5a6ef0aeaaab7153
Carlos Filipe Jr. <cfmtjr@gmail.com>
2014-05-11 21:15:50 -0300

    Adicionei os passos do algortimo como comentário, pra ficar mais fácil entender a separação.

--- src/metodopassovariavel/MetodoDePassoVariavel.java
+++ src/metodopassovariavel/MetodoDePassoVariavel.java
@@ -28,13 +28,18 @@ public class MetodoDePassoVariavel {
         boolean FLAG = true;
         BigDecimal t,w,h,r,sigma,
                    k1,k2,k3,k4,k5,k6;
+        
+        //Passo 1:
         t= a;
         w=alfa;
         h=hmax;                
         //SAIDA(t,w)
         resposta.add(new Ponto<>(t,w,BigDecimal.ZERO));        
+        
+        //Passo 2:
         while(FLAG){
             
+            //Passo 3:
             k1=calculador.K1(h, t, w);
             k2=calculador.K2(h, t, w, k1);
             k3=calculador.K3(h, t, w, k1, k2);
@@ -45,23 +50,28 @@ public class MetodoDePassoVariavel {
 //            BigDecimal k4=h*funcao(t+(12/13)*h, w+(1932/2197)*k1-(7200/2197)*k2+(7296/2197)*k3);
 //            BigDecimal k5=h*funcao(t+h, w+(439/216)*k1-(8)*k2+(3680/513)*k3-(845/4104)*k4);
 //            BigDecimal k6=h*funcao(t+(1/2)*h, w-(8/27)*k1-(2)*k2+(3544/2565)*k3-(1859/4104)*k4-(11/44)*k5);
-
+            
+            //Passo 4:
             r = calculador.R(h, k1, k3, k4, k5, k6);
             
-            //r<=TOL            
+            //Passo 5: r<=TOL            
             if(r.compareTo(TOL)<=0){
-                //Aproximação aceita
+                
+                //Passo 6: Aproximação aceita
                 t = t.add(h);
                 w=calculador.W(h, w, k1, k3, k4, k5);
+                
+                //Passo 7
                 //SAIDA(t,w,h)
                 resposta.add(new Ponto(t,w,h));
                // imprime(resposta);
                 //continue;
             }
             
+            //Passo 8:
             sigma= calculador.sigma(TOL, r);
             
-            //sigma <= 0.1
+            //Passo 9: sigma <= 0.1
             if(sigma.compareTo(BigDecimal.valueOf(0.1))<=0 ){
                 //h = h*0.1
                 h=h.multiply(BigDecimal.valueOf(0.1)); 
@@ -74,11 +84,11 @@ public class MetodoDePassoVariavel {
                 h=h.multiply(sigma);                
             }
             
-            //h>hmax
+            //Passo 10: h>hmax
             if(h.compareTo(hmax)>0) 
                 h=hmax;
             
-            //t>=b
+            //Passo 11: t>=b
             if(t.compareTo(b)>=0){               
                 FLAG=false;            
             }
@@ -92,6 +102,7 @@ public class MetodoDePassoVariavel {
                 //Exibir mensagem de erro;
             }        
         }
+        //Passo 12:
         return resposta;
     }
 
