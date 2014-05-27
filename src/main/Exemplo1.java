ac385a4b9a8659b93267f08330442d0efa8413d7
Thadeu Jose <Thadeu Jose@172.16.0.177>
2014-05-27 17:21:23 -0300

    Adicionamos uma função que constara como representação pratica para o relatório, pois esta representa bem a diminuição do H quando a função varia muito.

--- src/main/Exemplo1.java
+++ src/main/Exemplo1.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import metodopassovariavel.IFuncao;
 
 /**
@@ -12,6 +13,10 @@ public class Exemplo1 implements IFuncao{
     @Override
     public BigDecimal calcula(BigDecimal x, BigDecimal y) {
         // y' = y - x^2 + 1
-        return y.subtract(x.pow(2)).add(new BigDecimal(1));               
+        //return y.subtract(x.pow(2)).add(new BigDecimal(1));
+        //x^3+((x^4)*y)
+        return x.pow(3).add(x.pow(4).multiply(y));
+        //sin(x^2)
+        //return new BigDecimal(Math.sin(x.pow(2).doubleValue()));    
     }
 }
